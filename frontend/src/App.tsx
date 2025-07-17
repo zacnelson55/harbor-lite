c83d5e2712fa060d8bc8ea549174288eb18a5232
zacnelson55 <zacharyvnelson@gmail.com>
2025-07-17 14:40:10 -0700

    remove react import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { supabase } from './supabaseClient';
 import './App.css';
 import harborLogo from './assets/harbor-logo.svg';
